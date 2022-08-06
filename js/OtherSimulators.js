fb640c07102bf953e7da4df366ac7c00c35f4a97
Shivamshuklacvam <88248044+Shivamshuklacvam@users.noreply.github.com>
2022-08-06 20:33:12 +0530

    Initial commit of all files to the repository

---
+++ js/OtherSimulators.js
@@ -0,0 +1,67 @@
+const simData1=[
+  {
+    srNo:1,
+    collegeCode:'',
+    stuName:"",
+    simuName:"Tyro Learning  (Advance E-learning Portal)",
+    discription:'Shivam Shukla , Lavish Chaddha , Kanhaiya Agrawal , Devarshi Gupta',
+    ref:'https://github.com/devarshigupta457/Tyro-Learning',
+    stuPic:'../imgs/user.png',
+    CprgType:true,
+  },
+  {
+      srNo:2,
+      collegeCode:'',
+      stuName:"",
+      simuName:"Resume Builder",
+      discription:'Aman Kumar',
+      ref:'https://github.com/iamamanprajapati/resume-builder-app',
+      stuPic:'../imgs/user.png',
+      CprgType:true,
+  },
+  {
+    srNo:3,
+    collegeCode:'',
+    stuName:"",
+    simuName:"IET AGRA SIMULATOR",
+    discription:'Ahmed Raza , Vidit Patsariya , Vivek Pal',
+    ref:'https://github.com/AhmedRazacs/IETAgra',
+    stuPic:'../imgs/user.png',
+    CprgType:true,
+},
+{
+  srNo:4,
+  collegeCode:'',
+  stuName:"",
+  simuName:"Exam Portal",
+  discription:'Deepak kumar , Raghwendra pratap singh , Shivam bhaskar ,',
+  ref:'https://gilded-valkyrie-ae3dd2.netlify.app/',
+  stuPic:'../imgs/user.png',
+  CprgType:true,
+},
+{
+  srNo:4,
+  collegeCode:'',
+  stuName:"",
+  simuName:"text_summarizer",
+  discription:'Anshika Gupta,',
+  ref:'https://github.com/ans-web07/Anshika_text_summarizer',
+  stuPic:'../imgs/user.png',
+  CprgType:true,
+},
+{
+  srNo:4,
+  collegeCode:'',
+  stuName:"",
+  simuName:"Face Swapping using deep privacy",
+  discription:'Harsh Srivastav , Kratika Dhangar , Jyoti , Aniket Kumar',
+  ref:'https://github.com/KratikaDhangar/deep_privacy',
+  stuPic:'../imgs/user.png',
+  CprgType:true,
+},
+
+
+]
+
+
+export {simData1};
